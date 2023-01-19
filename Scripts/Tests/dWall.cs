559b09c136bb9b687d8f12d2d5f0845ea77d1ebd
sqrt71 <dimam2567@gmail.com>
2023-01-19 20:00:56 +0300

    newLevel

---
+++ Scripts/Tests/dWall.cs
@@ -0,0 +1,31 @@
+﻿using Scripts.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scripts.Tests
+{
+    public class dWall : Actor
+    {
+        private RigidBodyComponent rigidBody;
+        private StaticMeshRenderer staticMesh;
+
+        public dWall()
+        {
+
+        }
+
+        protected internal override void Init()
+        {
+            rigidBody = AddComponent<RigidBodyCubeComponent>("RigidBodyCubeComponent");
+            rigidBody.SetType(RigidBodyType.Dynamic);
+            rigidBody.SetUsage(RigidBodyUsage.CollisionsAndPhysics);
+            rigidBody.SetCollisionShape(CollisionShape.Box);
+
+            staticMesh = AddComponent<StaticMeshRenderer>("StaticMeshRenderer");
+            staticMesh.SetMeshPath("../Assets/box.fbx/Cube");
+        }
+    }
+}
